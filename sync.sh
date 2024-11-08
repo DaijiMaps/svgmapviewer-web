a4c6eaf057247d7c6357e3355a06d7b1209dc7f8
Masao Uebayashi <uebayasi@tombiinc.com>
2024-11-08 21:57:27 +0900

    Horyuji

--- sync.sh
+++ sync.sh
@@ -1,4 +1,9 @@
-( cd public/demos; ls -1 ) |
+args="$@"
+if [ -z "${args}" ]; then
+  args=$( cd public/demos; ls -1 )
+fi
+
+echo $args |
 while read mapId; do
   pushd ../svgmapviewer-demos/packages/demos/${mapId}
   pnpx vite build --base /demos/${mapId}
