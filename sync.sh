32ed0af4e84c428ea83848d3dfe9d2a4132da0e5
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-02 22:07:43 +0900

    xxx

--- sync.sh
+++ sync.sh
@@ -4,9 +4,11 @@ if [ -z "${args}" ]; then
 fi
 echo mapIds: $args
 
+#vite_build_args='--mode development'
+
 for mapId in $args; do
   pushd ../svgmapviewer-demos/packages/demos/${mapId}
-  pnpx vite build --base ''
+  pnpx vite build --base '' $vite_build_args
   popd
   rsync -avz --delete ../svgmapviewer-demos/packages/demos/${mapId}/dist/ ./public/demos/${mapId}/
 done
