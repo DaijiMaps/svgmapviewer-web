0ae6b21fe8975a82a29f0861b97f3f456bc992e0
Masao Uebayashi <uebayasi@tombiinc.com>
2025-05-27 14:23:40 +0900

    simply use pnpm build

--- sync.sh
+++ sync.sh
@@ -8,7 +8,8 @@ echo mapIds: $args
 
 for mapId in $args; do
   pushd ../svgmapviewer-demos/packages/demos/${mapId}
-  pnpx vite build --base '' $vite_build_args
+  #pnpx vite build --base '' $vite_build_args
+  pnpm build
   popd
   rsync -avz --delete ../svgmapviewer-demos/packages/demos/${mapId}/dist/ ./public/demos/${mapId}/
 done
