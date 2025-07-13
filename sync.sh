df1bdf0c170e5fefcec516a54905e565ed6f16e6
Masao Uebayashi <uebayasi@tombiinc.com>
2025-07-13 17:03:57 +0900

    add ForbiddenCity + KozuShima + Sarushima + ShikineJima

--- sync.sh
+++ sync.sh
@@ -1,6 +1,7 @@
 args="$@"
 if [ -z "${args}" ]; then
-  args=$( cd public/demos; ls -1 )
+  #args=$( cd public/demos; ls -1 )
+  args=$( cd src/content/demos; grep -h mapId: *.mdx | sed -e 's,^mapId: ,,' )
 fi
 echo mapIds: $args
 
@@ -11,5 +12,6 @@ for mapId in $args; do
   #pnpx vite build --base '' $vite_build_args
   pnpm build
   popd
+  mkdir -p ./public/demos/${mapId}
   rsync -avz --delete ../svgmapviewer-demos/packages/demos/${mapId}/dist/ ./public/demos/${mapId}/
 done
